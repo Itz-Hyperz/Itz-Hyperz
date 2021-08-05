3d99eb5f574a270acab4be70878592adb6a1d4c3
ThatGuyHyperz <loganfgross@gmail.com>
2021-08-05 13:37:36 -0700

    for the meme

---
+++ 200iq.js
@@ -0,0 +1,10 @@
+async randomize(doing, langs, tools) {
+  let random1 = doing[Math.floor(doing.length * Math.random())];
+  let random2 = langs[Math.floor(langs.length * Math.random())];
+  let random3 = tools[Math.floor(tools.length * Math.random())];
+  return [random1, random2, random3];
+}
+
+module.exports = {
+  randomize: randomize
+}
